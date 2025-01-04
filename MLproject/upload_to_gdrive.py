56658e6e2bee92bfd14ca827c4b709d40f3c855b
rafyardhani <rafyardhani@gmail.com>
2025-01-04 19:01:11 +0700

    share folder

--- MLproject/upload_to_gdrive.py
+++ MLproject/upload_to_gdrive.py
@@ -3,15 +3,26 @@ import json
 from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
+
 # Mengambil kredensial dari secret GitHub (gunakan format json)
 creds = json.loads(os.environ["GDRIVE_CREDENTIALS"])  # Ambil JSON dari secret GitHub
-# Membuat kredensial dari file JSON
-credentials = Credentials.from_service_account_info(creds)
+
+# Membuat kredensial dari file JSON dan menambahkan scope
+credentials = Credentials.from_service_account_info(
+    creds,
+    scopes=["https://www.googleapis.com/auth/drive"]  # atau drive.file
+)
+
 # Membangun layanan Google Drive API
 service = build('drive', 'v3', credentials=credentials)
+
 # Membuat folder di Google Drive
-folder_metadata = {'name': 'mlruns', 'mimeType': 'application/vnd.google-apps.folder'}
+folder_metadata = {
+    'name': 'mlruns',
+    'mimeType': 'application/vnd.google-apps.folder'
+}
 folder = service.files().create(body=folder_metadata, fields='id').execute()
+
 # Upload file ke folder yang baru dibuat
 folder_id = folder.get('id')
 for root, dirs, files in os.walk('./mlruns'):
@@ -20,4 +31,20 @@ for root, dirs, files in os.walk('./mlruns'):
         file_metadata = {'name': file_name, 'parents': [folder_id]}
         media = MediaFileUpload(file_path, resumable=True)
         service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+
 print("Files uploaded successfully!")
+
+folder_id = "ID_FOLDER_YANG_INGIN_DISHARE"
+permission = {
+    'type': 'user',                         # user, group, domain, atau anyone
+    'role': 'writer',                       # owner, organizer, writer, atau reader
+    'emailAddress': 'email.pribadi@gmail.com'
+}
+
+service.permissions().create(
+    fileId=folder_id,
+    body=permission,
+    fields='id'
+).execute()
+
+print("berhasil membagikan folder ke")
