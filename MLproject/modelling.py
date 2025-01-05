5e89a85ecbceaa9dd42cbf6696e6b76e5384e4d8
rafyardhani <rafyardhani@gmail.com>
2025-01-05 16:27:28 +0700

    finale code

--- MLproject/modelling.py
+++ MLproject/modelling.py
@@ -2,7 +2,6 @@ import mlflow
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 import random
 import numpy as np
 import os
