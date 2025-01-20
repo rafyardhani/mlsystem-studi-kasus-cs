0f6ad2abf6b1c4f5aeacfd3b2f38f85a9bf6bf3c
rafyardhani <rafyardhani@gmail.com>
2025-01-20 14:26:58 +0700

    update oauth

--- dag.py
+++ dag.py
@@ -1,2 +1,7 @@
 import dagshub
 dagshub.init(repo_owner='rafyardhani', repo_name='mlsystem-studi-kasus-cs', mlflow=True)
+import mlflow
+with mlflow.start_run():
+  # Your training code here...
+  mlflow.log_metric('accuracy', 42)
+  mlflow.log_param('Param name', 'Value')
