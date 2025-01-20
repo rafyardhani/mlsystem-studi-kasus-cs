a443eada25ea0b8bef695ceabbcc5242fcd5907a
rafyardhani <rafyardhani@gmail.com>
2025-01-20 13:35:03 +0700

    dags new

--- MLproject/modelling.py
+++ MLproject/modelling.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
     input_example = X_train[0:5]
     n_estimators = int(sys.argv[1]) if len(sys.argv) > 1 else 505
     max_depth = int(sys.argv[2]) if len(sys.argv) > 2 else 37
-    dagshub.init(repo_owner='rafyardhani', repo_name='my-first-repo', mlflow=True)
+    # dagshub.init(repo_owner='rafyardhani', repo_name='my-first-repo', mlflow=True)
     mlflow.set_tracking_uri("https://dagshub.com/rafyardhani/my-first-repo.mlflow")
     mlflow.set_experiment("Latihan Credit Scoring")
     with mlflow.start_run():
