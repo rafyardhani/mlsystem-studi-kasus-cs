d006a5aee10cdbfd6538c1262e488f4585d77396
rafyardhani <rafyardhani@gmail.com>
2025-01-20 13:15:04 +0700

    tracking

--- MLproject/modelling.py
+++ MLproject/modelling.py
@@ -28,7 +28,7 @@ if __name__ == "__main__":
     n_estimators = int(sys.argv[1]) if len(sys.argv) > 1 else 505
     max_depth = int(sys.argv[2]) if len(sys.argv) > 2 else 37
     dagshub.init(repo_owner='rafyardhani', repo_name='my-first-repo', mlflow=True)
-
+    mlflow.set_tracking_uri("https://dagshub.com/rafyardhani/my-first-repo.mlflow")
     with mlflow.start_run():
         model = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth)
         model.fit(X_train, y_train)
